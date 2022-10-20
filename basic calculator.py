548eee346346949e967d42e1480b9e562c026470
Suri071 <112838478+Suri071@users.noreply.github.com>
2022-10-20 14:51:19 +0530

    Update basic calculator.py

--- basic calculator.py
+++ basic calculator.py
@@ -1,22 +1,37 @@
-print("Enter the expression")
-class Solution:
-    def calculate(self, s: str) -> int:
-        stack = []
-        def append(operator, operand):
-            if operator == "+":
-                stack.append(operand)
-            elif operator == "-":
-                stack.append(-operand)
-            elif operator == "*":
-                stack.append(stack.pop() * operand)
-            else:
-                stack.append(int(stack.pop() / operand))
-        operator, operand = "+", 0
-        for c in s:
-            if c in "0123456789":
-                operand = operand * 10 + int(c)
-            elif c in "+-*/":
-                append(operator, operand)
-                operator, operand = c, 0
-        append(operator, operand)
-        return sum(stack)
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    return x / y
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    
+    else:
+        print("Invalid Input")
